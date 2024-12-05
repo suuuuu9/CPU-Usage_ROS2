2271d3a6a6188e1c68999a58d183e7341ac2b30c
Suzuha Kiuchi <kiuchi.ju@gmail.com>
2024-12-05 12:33:17 +0900

    サービス実装

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,17 +1,32 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from person_msgs.srv import Query
 
 
 rclpy.init()
 node = Node("listener")
 
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %s" % msg)
-
-
 def main():
-    pub = node.create_subscription(Person, "person", cb, 10)
-    rclpy.spin(node)
+    client = node.create_client(Query, 'query')
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中')
+
+    req = Query.Request()
+    req.name = "木内涼葉"
+    future = client.call_async(req)
+
+    while rclpy.ok():
+        rclpy.spin_once(node)
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
+            
+            break
+    
+    node.destroy_node()
+    rclpy.shutdown()

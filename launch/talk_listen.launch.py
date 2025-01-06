3925e43ccbe3672abcbeffa074b28b507a0f1e31
Suzuha Kiuchi <kiuchi.ju@gmail.com>
2025-01-06 15:08:03 +0900

    パッケージ名修正

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -7,11 +7,11 @@ import launch_ros.actions
 def generate_launch_description():
 
     talker = launch_ros.actions.Node(
-        package='mypkg',
+        package='cpu_usage_ros2',
         executable='talker',
         )
     listener = launch_ros.actions.Node(
-        package='mypkg',
+        package='cpu_usage_ros2',
         executable='listener',
         output='screen'
         )
